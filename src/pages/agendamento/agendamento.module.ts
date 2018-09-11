d7725598859728da1db306c7d90b1a649d9e3605
joaodmayer <joao.mayer@diamondsistemas.com.br>
2018-09-11 20:10:25 -0300

    agendamento

--- src/pages/agendamento/agendamento.module.ts
+++ src/pages/agendamento/agendamento.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AgendamentoPage } from './agendamento';
+import { SharedModule } from '../../directives/shared.module';
 
 @NgModule({
   declarations: [
-    AgendamentoPage,
+    AgendamentoPage
   ],
   imports: [
     IonicPageModule.forChild(AgendamentoPage),
+    SharedModule
   ],
 })
 export class AgendamentoPageModule {}
