1e64f7d33c4600ea81bf75bd549c7fcd03df5cf9
Bojko <bojko1998@gmail.com>
2018-09-11 19:47:24 -0300

    Alteraçoes na tela visao geral, inclusao de graficos

--- src/pages/visao-geral/visao-geral.ts
+++ src/pages/visao-geral/visao-geral.ts
@@ -25,4 +25,30 @@ export class VisaoGeralPage {
     console.log('ionViewDidLoad VisaoGeralPage');
   }
 
+  tipoAtxAt =  {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Tipo de Atendimento', 'Atendimento'],
+      ['Teste Rápido',     28],
+      ['Preventivo',      26],
+      ['Consulta Médica',  12],
+      ['Eletrocardiograma', 14],
+      ['Ataque de animal',    20]
+    ],
+  }
+
+    AtxLotacao =  {
+      chartType: 'PieChart',
+      dataTable: [
+        ['Atendimento', 'Lotação'],
+        ['Acadêmico',     10],
+        ['DEINFO',      30],
+        ['DEMAT',  20],
+        ['Comunidade Externa', 15],
+        ['PRORH',    25]
+      ],
+      /* options: {title: 'Countries', allowHtml: true}, */
+   
+  }; 
+
 }
