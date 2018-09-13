1af07aa66c7605dac338aa4fd83a3a4b74fe1948
Bojko <bojko1998@gmail.com>
2018-09-13 16:48:25 -0300

    Alteraçoes e correçoes nos graficos

--- src/pages/visao-geral/visao-geral.ts
+++ src/pages/visao-geral/visao-geral.ts
@@ -29,26 +29,53 @@ export class VisaoGeralPage {
     chartType: 'PieChart',
     dataTable: [
       ['Tipo de Atendimento', 'Atendimento'],
-      ['Teste Rápido',     28],
-      ['Preventivo',      26],
-      ['Consulta Médica',  12],
-      ['Eletrocardiograma', 14],
-      ['Ataque de animal',    20]
+      ['Teste Rápido',     166],
+      ['Preventivo',      34],
+      ['Consulta Médica',  3],
+      ['Eletrocardiograma', 12],
+      ['Ataque de animal',    96]
     ],
-  }
-
+    options:{
+      chartArea:{left:'0%',top:'5%', width:'100%',height:'100%'},
+      is3D: false,
+      pieHole: 0,
+      fontSize:20,
+      enableInteractivity: true,
+      pieSliceText:'value',
+      legend:{position: 'labeled', maxLines: 2},
+      height: 500,
+      width: 900
+    }
+    }
+ 
     AtxLotacao =  {
       chartType: 'PieChart',
       dataTable: [
         ['Atendimento', 'Lotação'],
-        ['Acadêmico',     10],
-        ['DEINFO',      30],
-        ['DEMAT',  20],
-        ['Comunidade Externa', 15],
-        ['PRORH',    25]
+        ['Acadêmico',     94],
+        ['DEINFO',      33],
+        ['DEMAT',  12],
+        ['Comunidade Externa', 85],
+        ['PRORH',    15],
+        ['SCATE',     32],
+        ['NUTEAD',      4],
+        ['DEFIS',  3],
+        ['DEDIR', 12],
+        ['DEBIOGEM',    67]
       ],
-      /* options: {title: 'Countries', allowHtml: true}, */
+      options:{
+        colors:['#F4C2C2'/* rosinnha */,'#A9A9A9'/* cinza claro */,'#FF0800'/* vermelho */,'#66FF00' /* verde claro */,'#013220'/* verde escuro */, '#00FFFF'/* ciano */,'#0072BB'/* azul */,'#FF8C00' /* laranja */,'#967117' /* beje */,'#FFBF00'/* amarelo */,'#26428B'/* azul escuro */,'#7C0A02' /* marrom */,'#8DB600' /* verde opaco */,'#BF00FF'/* roxo */, '#F400A1'/* rosa */,'#E30022'/* vermelho */,'#555555'/* cinze escuro */,'#1B1B1B'/* preto */,'#3B2F2F'/* cinza */],
+        chartArea:{left:'0%',top:'5%', width:'100%',height:'100%'},
+        is3D: false,
+        pieHole: 0,
+        fontSize:20,
+        enableInteractivity: true,
+        pieSliceText:'value',
+        legend:{position: 'labeled', maxLines: 2},
+        height: 500,
+        width: 900
+      }
+      }
    
-  }; 
+  };
 
-}
